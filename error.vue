d471eec1109456d2235e5400b43ad0c523a4c4e2
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2023-01-18 19:39:23 +0200

    Error 404 Page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex h-screen justify-center items-center flex-col">
+    <h1 class="text-9xl">404</h1>
+    <p class="mt-7 text-4xl">Page not Found</p>
+    <button
+      @click="handleReturn"
+      class="rounded mt-7 text-2xl bg-indigo-500 px-7 py-4 text-white"
+    >
+      Go Back
+    </button>
+  </div>
+</template>
+
+<script setup>
+const handleReturn = () => {
+  navigateTo("/");
+};
+</script>
